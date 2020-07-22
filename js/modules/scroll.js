60adbc06692ca061ecb11e324c0235dd295462c9
jack1-2-3 <jack.programming123@gmail.com>
2020-07-22 13:27:33 +0900

    ScrollObserverクラスの記述

---
+++ js/modules/scroll.js
@@ -0,0 +1,38 @@
+// スクロール感知のクラス
+class ScrollObserver {
+    constructor(els, cb, options) {
+        this.els = document.querySelectorAll(els);
+        this.cb = cb;
+        this.options = Object.assign({
+            root: null,
+            rootmargin: '0px',
+            threshold: 0.3,
+            once: true
+        }, options);
+        this.once = this.options.once;
+        this.init = this._init();
+    }
+
+    // 初期設定
+    _init() {
+        // コールバック
+        const callback = (entries, observer) => {
+            entries.forEach(entry => {
+                if(entry.isIntersecting) {
+                    // cbコールバック
+                    this.cb(entry.target, true);
+                    if(this.once) {
+                        observer.unobserve(entry.target);
+                    }
+                } else {
+                    this.cb(entry.target,false);
+                }
+            })
+        }
+
+        // IntersectionObserverのインスタンス呼び出し
+        const io = new IntersectionObserver(callback, this.options);
+        // 要素監視
+        this.els.forEach(el => io.observe(el));
+    }
+}
